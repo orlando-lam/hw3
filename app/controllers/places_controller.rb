8ddb841808b23b77b5b8686453e7bd8ff4c14b48
orlando-lam <orlando.lam@kellogg.northwestern.edu>
2022-05-17 23:03:33 +0000

    v2

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -15,8 +15,9 @@ class PlacesController < ApplicationController
 
     def create
         @place = Place.new
-        
+        @place["name"] = params["place"]["name"]
         @place.save
+        redirect_to "/places"
     end
 
 end
